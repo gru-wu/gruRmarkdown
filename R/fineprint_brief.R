e6bd35d5a3048ed6336542f8fd660f476236e81e
vwmaus <vwmaus@gmail.com>
2018-11-06 19:39:47 +0100

    Add cross-referencing

--- R/fineprint_brief.R
+++ R/fineprint_brief.R
@@ -15,24 +15,24 @@
 #'
 #' rmarkdown::draft(file = "my_brief.Rmd", create_dir = TRUE, template = "fineprint_brief", package = "gruRmarkdown",  edit = FALSE)
 #'
-#' # Render brief PDF
-#' rmarkdown::render(input = "my_brief/my_brief.Rmd", output_format = "gruRmarkdown::fineprint_brief")
-#'
-#' # Render brief HTML
-#' rmarkdown::render(input = "my_brief/my_brief.Rmd", output_format = "html_document")
+#' # Render brief PDF and HTMl
+#' rmarkdown::render(input = "my_brief/my_brief.Rmd",
+#'   output_format = c("gruRmarkdown::fineprint_brief", "bookdown::html_document2"),
+#'   output_options = list(number_sections = FALSE))
 #'
 #' }
 #'
 #' @export
 fineprint_brief = function(..., keep_tex = TRUE, toc = FALSE, number_sections = FALSE) {
-  res = rmarkdown::pdf_document(..., keep_tex = keep_tex, toc = toc, number_sections = number_sections,
+  res = bookdown::pdf_document2(..., keep_tex = keep_tex, toc = toc, number_sections = number_sections,
                                 template = .find_file("fineprint_brief", file.path("resources", "template.tex")))
-  res$inherits = "pdf_document"
+
+  res$inherits = "bookdown::pdf_document2"
   res$knitr$opts_knit$out.format = "sweave"
   res$knitr$opts_chunk$prompt = TRUE
   res$knitr$opts_chunk$comment = NA
   res$knitr$opts_chunk$highlight = FALSE
-  #res$knitr$opts_chunk$fig.align = "center"
+  res$knitr$opts_chunk$fig.align = "center"
   res
 }
 
