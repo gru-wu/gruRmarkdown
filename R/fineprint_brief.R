5bb2e09e6b66a1a212cf147d220155a67a0cac87
vwmaus <vwmaus@gmail.com>
2018-11-07 14:20:16 +0100

    Add simple FINEPRINT html template

--- R/fineprint_brief.R
+++ R/fineprint_brief.R
@@ -1,8 +1,8 @@
-#' FINEPRINT Brief template
+#' FINEPRINT Brief template for pdf output
 #'
 #' @inheritParams bookdown::pdf_document2
 #' @inheritParams rmarkdown::pdf_document
-#' @param ... Arguments to \code{rmarkdown::pdf_document}
+#' @param ... Arguments to \code{bookdown::pdf_document2}
 #'
 #' @return R Markdown output format to pass to
 #'   \code{\link[rmarkdown:render]{render}}
@@ -19,13 +19,13 @@
 #'
 #' # Render brief PDF and HTMl
 #' rmarkdown::render(input = "my_brief/my_brief.Rmd",
-#'   output_format = c("gruRmarkdown::fineprint_brief", "bookdown::html_document2"),
+#'   output_format = c("gruRmarkdown::pdf_fineprint_brief", "gruRmarkdown::html_fineprint_brief"),
 #'   output_options = list(number_sections = FALSE))
 #'
 #' }
 #'
 #' @export
-fineprint_brief = function(..., keep_tex = TRUE, toc = FALSE, number_sections = FALSE) {
+pdf_fineprint_brief <- function(..., keep_tex = TRUE, toc = FALSE, number_sections = FALSE) {
   res = bookdown::pdf_document2(..., keep_tex = keep_tex, toc = toc, number_sections = number_sections,
                                 template = .find_file("fineprint_brief", file.path("resources", "template.tex")))
 
@@ -36,6 +36,39 @@ fineprint_brief = function(..., keep_tex = TRUE, toc = FALSE, number_sections = 
   res$knitr$opts_chunk$highlight = FALSE
   res$knitr$opts_chunk$fig.align = "center"
   res
+}
+
+
+#' FINEPRINT Brief template for html output
+#'
+#' @inheritParams bookdown::html_document2
+#' @inheritParams rmarkdown::html_document
+#' @param ... Arguments to \code{bookdown::html_document2}
+#'
+#' @return R Markdown output format to pass to
+#'   \code{\link[rmarkdown:render]{render}}
+#'
+#' @importFrom bookdown html_document2
+#'
+#' @examples
+#'
+#' \dontrun{
+#' library(gruRmarkdown)
+#'
+#' rmarkdown::draft(file = "my_brief.Rmd", create_dir = TRUE,
+#'   template = "pdf_fineprint_brief", package = "gruRmarkdown",  edit = FALSE)
+#'
+#' # Render brief PDF and HTMl
+#' rmarkdown::render(input = "my_brief/my_brief.Rmd",
+#'   output_format = c("gruRmarkdown::pdf_fineprint_brief", "gruRmarkdown::html_fineprint_brief"),
+#'   output_options = list(number_sections = FALSE))
+#'
+#' }
+#'
+#' @export
+html_fineprint_brief <- function(..., toc = FALSE, theme = NULL, number_sections = FALSE) {
+  bookdown::html_document2(..., toc = toc, theme = theme, number_sections = number_sections,
+                           template = .find_file("fineprint_brief", file.path("resources", "template.html")))
 }
 
 .find_file = function(template, file) {
