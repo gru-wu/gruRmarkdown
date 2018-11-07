04756a71e7787b72e071078e2ef30c0fad545bf4
vwmaus <vwmaus@gmail.com>
2018-11-07 14:23:15 +0100

    Document

--- R/fineprint_brief.R
+++ R/fineprint_brief.R
@@ -14,13 +14,13 @@
 #' \dontrun{
 #' library(gruRmarkdown)
 #'
+#' # Create draft
 #' rmarkdown::draft(file = "my_brief.Rmd", create_dir = TRUE,
 #'   template = "fineprint_brief", package = "gruRmarkdown",  edit = FALSE)
 #'
 #' # Render brief PDF and HTMl
 #' rmarkdown::render(input = "my_brief/my_brief.Rmd",
-#'   output_format = c("gruRmarkdown::pdf_fineprint_brief", "gruRmarkdown::html_fineprint_brief"),
-#'   output_options = list(number_sections = FALSE))
+#'   output_format = c("gruRmarkdown::pdf_fineprint_brief", "gruRmarkdown::html_fineprint_brief"))
 #'
 #' }
 #'
@@ -55,13 +55,13 @@ pdf_fineprint_brief <- function(..., keep_tex = TRUE, toc = FALSE, number_sectio
 #' \dontrun{
 #' library(gruRmarkdown)
 #'
+#' # Create draft
 #' rmarkdown::draft(file = "my_brief.Rmd", create_dir = TRUE,
-#'   template = "pdf_fineprint_brief", package = "gruRmarkdown",  edit = FALSE)
+#'   template = "fineprint_brief", package = "gruRmarkdown",  edit = FALSE)
 #'
 #' # Render brief PDF and HTMl
 #' rmarkdown::render(input = "my_brief/my_brief.Rmd",
-#'   output_format = c("gruRmarkdown::pdf_fineprint_brief", "gruRmarkdown::html_fineprint_brief"),
-#'   output_options = list(number_sections = FALSE))
+#'   output_format = c("gruRmarkdown::pdf_fineprint_brief", "gruRmarkdown::html_fineprint_brief"))
 #'
 #' }
 #'
